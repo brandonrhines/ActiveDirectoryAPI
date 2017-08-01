0b6fa59fbebb9aa0f5ace072f96905cc35e87975
Brandon Rhines <brhines@foundationsoft.com>
2017-08-01 09:55:52 -0400

    Add Get, Post, Put, Patch, and Delete with validation

--- ActiveDirectoryAPI/Controllers/GroupsController.cs
+++ ActiveDirectoryAPI/Controllers/GroupsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ActiveDirectoryAPI.Model;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +21,7 @@ namespace ActiveDirectoryAPI.Controllers
             }
             return Ok(user.Groups);
         }
-        [HttpGet("{userID}/{groupID}")]
+        [HttpGet("{userID}/groups/{groupID}", Name = "GetGroup")]
         public IActionResult GetGroup(int userID, int groupID)
         {
             var user = UserDataStore.Current.Users.FirstOrDefault(u => u.ID == userID);
@@ -34,6 +36,157 @@ namespace ActiveDirectoryAPI.Controllers
                 return NotFound();
             }
             return Ok(group);
+        }
+        [HttpPost("{userID}/groups")]
+        public IActionResult CreateGroup(int userID, [FromBody] GroupForCreation group)
+        {
+            if(group == null)
+            {
+                return BadRequest();
+            }
+
+            if(group.Description == group.GroupName)
+            {
+                ModelState.AddModelError("Description", "The description should be different than the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = UserDataStore.Current.Users.FirstOrDefault(u => u.ID == userID);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            //-- to be changed
+            var maxGroupID = UserDataStore.Current.Users.SelectMany(
+                u => u.Groups).Max(g => g.ID);
+
+            var finalGroup = new Group()
+            {
+                ID = ++maxGroupID,
+                GroupName = group.GroupName,
+                Description = group.Description
+            };
+
+            user.Groups.Add(finalGroup);
+
+            return CreatedAtRoute("GetGroup", new {
+                userID = userID, groupID = finalGroup.ID}, finalGroup);
+        }
+
+        [HttpPut("{userID}/groups/{groupID}")]
+        public IActionResult UpdateGroup(int userID, int groupID, [FromBody] GroupForUpdate group)
+        {
+            if (group == null)
+            {
+                return BadRequest();
+            }
+
+            if (group.Description == group.GroupName)
+            {
+                ModelState.AddModelError("Description", "The description should be different than the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = UserDataStore.Current.Users.FirstOrDefault(u => u.ID == userID);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var groupFromStore = user.Groups.FirstOrDefault(g => g.ID == groupID);
+            if (groupFromStore == null)
+            {
+                return NotFound();
+            }
+
+            groupFromStore.GroupName = group.GroupName;
+            groupFromStore.Description = group.Description;
+
+            //typically return no content with put
+            return NoContent();
+        }
+
+        [HttpPatch("{userID}/groups/{groupID}")]
+        public IActionResult PartialUpdateGroup(int userID, int groupID,
+            [FromBody] JsonPatchDocument<GroupForUpdate> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var user = UserDataStore.Current.Users.FirstOrDefault(u => u.ID == userID);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var groupFromStore = user.Groups.FirstOrDefault(g => g.ID == groupID);
+            if (groupFromStore == null)
+            {
+                return NotFound();
+            }
+
+            var groupToPatch =
+                new GroupForUpdate()
+                {
+                    GroupName = groupFromStore.GroupName,
+                    Description = groupFromStore.Description
+                };
+
+            patchDoc.ApplyTo(groupToPatch, ModelState);
+
+            //checks patchDoc model state not GroupForUpdate model state
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (groupToPatch.Description == groupToPatch.GroupName)
+            {
+                ModelState.AddModelError("Description", "The description should be different than the name.");
+            }
+
+            TryValidateModel(groupToPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            groupFromStore.GroupName = groupToPatch.GroupName;
+            groupFromStore.Description = groupToPatch.Description;
+
+            return NoContent();
+        }
+        [HttpDelete("{userID}/groups/{groupID}")]
+        public IActionResult DeleteGroup(int userID, int groupID)
+        {
+            var user = UserDataStore.Current.Users.FirstOrDefault(u => u.ID == userID);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var group = user.Groups.FirstOrDefault(g => g.ID == groupID);
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            user.Groups.Remove(group);
+
+            return NoContent();
+
         }
     }
 }
---
+++ ActiveDirectoryAPI/Model/GroupForCreation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryAPI.Model
+{
+    public class GroupForCreation
+    {
+        [Required(ErrorMessage = "Group Name is required.")]
+        [MaxLength(50)]
+        public string GroupName { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
